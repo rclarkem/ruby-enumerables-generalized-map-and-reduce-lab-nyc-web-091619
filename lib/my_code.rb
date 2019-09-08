6fd13191882af4c1e21633adc59982dd0ea84536
Rei M <r.clarkemitchell@gmail.com>
2019-09-08 03:06:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(source_array)
+newArr = []
+source_array.length.times do |index|
+newArr << yield(source_array[index])
+
+end
+newArr
+end
+
+
+def reduce(source_array, starting_v = 0)
+sum = starting_v
+source_array.length.times do |index|
+sum = yield(source_array[index], sum)
+#  sum = source_array[index]
+end
+sum || false
+end
