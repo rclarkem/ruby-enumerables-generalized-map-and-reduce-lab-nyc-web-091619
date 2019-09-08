841c791ed65cea789315b92c654c78799deb77a6
Rei M <r.clarkemitchell@gmail.com>
2019-09-08 03:07:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce(source_array, starting_v = 0)
 sum = starting_v
 source_array.length.times do |index|
 sum = yield(source_array[index], sum)
-#  sum = source_array[index]
+#  sum = source_array[index] 
 end
 sum || false
 end
